ae451d64345ad728e75dd913ddc5f23fec96446a
lily10455 <lily.vallois@gmail.com>
2025-06-23 00:17:38 +0200

    Leaderboard ON

--- app/src/main/java/clem/project/buzz/data/local/AppDatabase.kt
+++ app/src/main/java/clem/project/buzz/data/local/AppDatabase.kt
@@ -5,19 +5,24 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Score::class], version = 1, exportSchema = false)
+@Database(entities = [Score::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun scoreDao(): ScoreDao
 
     companion object {
+
         @Volatile private var INSTANCE: AppDatabase? = null
+
         fun get(context: Context): AppDatabase =
             INSTANCE ?: synchronized(this) {
                 Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "buzz-db"
-                ).build().also { INSTANCE = it }
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
             }
     }
 }
